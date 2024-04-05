31bc6a3c914000b8a1ac65ec7b34e415542840e8
Kushagra Khatwani <kushagrakhatwani19@gmail.com>
2024-04-05 18:39:21 -0400

    Updated the signup form to add more details

--- Roomio/accounts/models.py
+++ Roomio/accounts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=20, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='custom_user_set',
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='custom_user_permissions',
+    )
--- Roomio/accounts/views.py
+++ Roomio/accounts/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import UserRegistrationForm
+from .models import User
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserRegistrationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
